2e6811caf1fe5b24de4d9e0665d7e5076bfa614c
Your Name <you@example.com>
2022-02-03 17:38:53 +0200

    axios commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import Fetch from './Fetch';
+//import Fetch from './Fetch';
+import axios from 'axios';
+import React , { useState, useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([]); 
+
+  useEffect(() => {
+    axios.get('http://hn.algolia.com/api/v1/search?query=react')
+    .then((res) => {
+      let input = res.data.hits;
+      setPosts(input);
+      console.log(input);
+  })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hacker News</h1>
-        <Fetch />
       </header>
     </div>
   );
--- src/Fetch.js
+++ src/Fetch.js
@@ -1,24 +1,24 @@
 
-const ul = document.getElementById("users"); // Get the element where we will place our users
-const url = "https://randomuser.me/api/?results=10"; // Get 10 random users
+// const ul = document.getElementById("users"); 
+// const url = "https://randomuser.me/api/?results=10"; 
 
-export default function Fetch(data)  {
-  const users = data.results;
-  console.debug({ users });
+// export default function Fetch(data)  {
+//   const users = data.results;
+// //   console.debug({ users });
 
-  return users.map((user) => {
-    return (ul.innerHTML += `
-    <li>
-      <img src="${user.picture.medium}" />
-      <p>${user.name.first} ${user.name.last}</p>
-      <div>${user.email}</div>
-    </li>`);
-  });
-};
+//   return users.map((user) => {
+//     return (ul.innerHTML += `
+//     <li>
+//       <img src="${user.picture.medium}" />
+//       <p>${user.name.first} ${user.name.last}</p>
+//       <div>${user.email}</div>
+//     </li>`);
+//   });
+// };
 
 
-fetch(url)
-.then(response => response.json())
-.then(data => Fetch(data))
-.catch(error => console.log("Error"));
+// fetch(url)
+// .then(response => response.json())
+// .then(data => Fetch(data))
+// .catch(error => console.log("Error"));
 
