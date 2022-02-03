5889af106e48caeda5d59c0c36228978487013f3
Your Name <you@example.com>
2022-02-03 16:42:23 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Fetch from './Fetch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hacker News</h1>
+        <Fetch />
       </header>
     </div>
   );
---
+++ src/Fetch.js
@@ -0,0 +1,24 @@
+
+const ul = document.getElementById("users"); // Get the element where we will place our users
+const url = "https://randomuser.me/api/?results=10"; // Get 10 random users
+
+export default function Fetch(data)  {
+  const users = data.results;
+  console.debug({ users });
+
+  return users.map((user) => {
+    return (ul.innerHTML += `
+    <li>
+      <img src="${user.picture.medium}" />
+      <p>${user.name.first} ${user.name.last}</p>
+      <div>${user.email}</div>
+    </li>`);
+  });
+};
+
+
+fetch(url)
+.then(response => response.json())
+.then(data => Fetch(data))
+.catch(error => console.log("Error"));
+
